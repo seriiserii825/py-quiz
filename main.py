1fcb366d7a95f7f03b3a96110d27a9b3d2d1fd7d
serii burduja <seriiburduja@gmail.com>
2024-10-18 18:26:38 +0300

    feat: added api

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import requests
 from Question import Question
 from QuizBrain import QuizBrain
-from data import question_data
+# from data import question_data
+
+url = 'https://opentdb.com/api.php?amount=10&type=boolean'
+
+questions = requests.get(url).json()['results']
+question_data = []
+for item in questions:
+    try:
+        question_data.append({
+            "text": item['question'],
+            "answer": item['correct_answer']
+            })
+    except KeyError:
+        print(f'KeyError: {item}')
 
 question_bank = []
 for item in question_data:
     question = Question(item['text'], item['answer'])
     question_bank.append(question)
-    
+
 quiz = QuizBrain(question_bank)
 
 while quiz.stillHasQuestions():
